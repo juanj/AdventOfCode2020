18656170c68bfcd720b45193e2275f4a44253768
Juan Jose <1958175+juanj@users.noreply.github.com>
2020-12-21 08:11:31 -0500

    Day 21 part 2

--- Sources/AdventOfCode2020Lib/Day 21/Day21Executer.swift
+++ Sources/AdventOfCode2020Lib/Day 21/Day21Executer.swift
@@ -19,5 +19,6 @@ public struct Day21Executer: Executer {
             return result + list.ingredients.reduce(0, { $0 + (safeIngredients.contains($1) ? 1 : 0) })
         }
         print("The number of times safe ingredients apper is: \(count)")
+        print("The canonical dangerous ingredient list is: \(allergens.sorted { $0.translatedName < $1.translatedName }.map(\.name).joined(separator: ","))")
     }
 }
