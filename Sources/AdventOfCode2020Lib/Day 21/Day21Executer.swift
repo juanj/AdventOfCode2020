2fed18f5da6c0e740b918f3d307d5180004cfdfd
Juan Jose <1958175+juanj@users.noreply.github.com>
2020-12-21 08:18:25 -0500

    Fix typo

--- Sources/AdventOfCode2020Lib/Day 21/Day21Executer.swift
+++ Sources/AdventOfCode2020Lib/Day 21/Day21Executer.swift
@@ -18,7 +18,7 @@ public struct Day21Executer: Executer {
         let count = lists.reduce(0) { result, list -> Int in
             return result + list.ingredients.reduce(0, { $0 + (safeIngredients.contains($1) ? 1 : 0) })
         }
-        print("The number of times safe ingredients apper is: \(count)")
+        print("The number of times safe ingredients appear is: \(count)")
         print("The canonical dangerous ingredient list is: \(allergens.sorted { $0.translatedName < $1.translatedName }.map(\.name).joined(separator: ","))")
     }
 }
