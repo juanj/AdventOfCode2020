5cda0e9847bfc2814698f68769f84b8fd7bfdf89
Juan Jose <1958175+juanj@users.noreply.github.com>
2020-12-22 10:07:58 -0500

    Day 22 part 2 (slow)

--- Sources/AdventOfCode2020Lib/Day 22/CardUtils.swift
+++ Sources/AdventOfCode2020Lib/Day 22/CardUtils.swift
@@ -8,6 +8,15 @@
 import Foundation
 
 public struct CardUtils {
+    public enum Player {
+        case player1, player2
+    }
+
+    public struct GameState: Hashable {
+        var player1: [Int]
+        var player2: [Int]
+    }
+
     public init() {}
     public func load(from file: String) -> (player1: [Int], player2: [Int]) {
         let decks = file.trimmingCharacters(in: .whitespacesAndNewlines)
@@ -40,14 +49,45 @@ public struct CardUtils {
         return decks
     }
 
-    public func score(decks: (player1: [Int], player2: [Int])) -> Int {
-        let deck: [Int]
-        if decks.player1.count > 0 {
-            deck = decks.player1
-        } else {
-            deck = decks.player2
+    public func playUntilWin(decks: GameState, seenStates: [GameState: Bool] = [:]) -> (winner: Player, deck: [Int]) {
+        var seenStates = seenStates
+        var currentState = decks
+        while currentState.player1.count > 0 && currentState.player2.count > 0 {
+            if seenStates[currentState] == true {
+                return (winner: .player1, deck: currentState.player1)
+            }
+            seenStates[currentState] = true
+            let player1Card = currentState.player1.removeFirst()
+            let player2Card = currentState.player2.removeFirst()
+
+            if currentState.player1.count >= player1Card && currentState.player2.count >= player2Card {
+                let subState = GameState(player1: Array(currentState.player1.prefix(player1Card)), player2: Array(currentState.player2.prefix(player2Card)))
+                if playUntilWin(decks: subState, seenStates: seenStates).winner == .player1 {
+                    currentState.player1.append(player1Card)
+                    currentState.player1.append(player2Card)
+                } else {
+                    currentState.player2.append(player2Card)
+                    currentState.player2.append(player1Card)
+                }
+            } else {
+                if player1Card > player2Card {
+                    currentState.player1.append(player1Card)
+                    currentState.player1.append(player2Card)
+                } else {
+                    currentState.player2.append(player2Card)
+                    currentState.player2.append(player1Card)
+                }
+            }
         }
 
+        if currentState.player1.count > 0 {
+            return (winner: .player1, deck: currentState.player1)
+        } else {
+            return (winner: .player2, deck: currentState.player2)
+        }
+    }
+
+    public func score(deck: [Int]) -> Int {
         return deck
             .reversed()
             .enumerated()
--- Sources/AdventOfCode2020Lib/Day 22/Day22Executer.swift
+++ Sources/AdventOfCode2020Lib/Day 22/Day22Executer.swift
@@ -16,7 +16,12 @@ public struct Day22Executer: Executer {
         while decks.player1.count > 0 && decks.player2.count > 0 {
             decks = utils.oneRound(decks: decks)
         }
+        let deck = decks.player1.count > 0 ? decks.player1 : decks.player2
+        print("The score after plaing Combat is \(utils.score(deck: deck))")
 
-        print("The score after plaing Combat is \(utils.score(decks: decks))")
+        let recursiveDeck = utils.load(from: input)
+        let gameState = CardUtils.GameState(player1: recursiveDeck.player1, player2: recursiveDeck.player2)
+        let winner = utils.playUntilWin(decks: gameState)
+        print("The score after plaing Combat is \(utils.score(deck: winner.deck))")
     }
 }
